24e2f6874f3e63e69b05ee073779caac29db025e
roy ledford <bluesixty@gmail.com>
2018-03-09 11:38:09 -0500

    add some new utils

---
+++ src/debounce.js
@@ -0,0 +1,17 @@
+/**
+* Debounce function from Wes Bos 30 days of javascript
+*/
+function debounce(func, wait = 20, immediate = true) {
+  var timeout;
+  return function() {
+    var context = this, args = arguments;
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
